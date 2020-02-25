67ae723162dd4e787771277efbeca516a43742c3
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-02-25 11:03:59 +0100

    modif affichage Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome at Abbe-Biere
                 </div>
 
                 <div class="links">
