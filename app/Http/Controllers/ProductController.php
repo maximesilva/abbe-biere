47aea74f0a17f5a2bf0468a9d671dbd873b32676
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-02-26 14:45:53 +0100

    Route Product + Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show($id){
+        return view ('product', ['id'=> $id]);
+    }
+}
