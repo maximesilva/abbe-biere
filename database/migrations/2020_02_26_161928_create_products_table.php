31e715cc17581c081bc9329d62b54c5050cc32e5
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-02-26 16:33:42 +0100

    migration product table

---
+++ database/migrations/2020_02_26_161928_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->char('name', 250);
+            $table->text('description');
+            $table->decimal('price');
+            $table->integer('volume');
+            $table->decimal('vat', 5, 2);
+            $table->integer('stock');
+            $table->decimal('weight', 7, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
