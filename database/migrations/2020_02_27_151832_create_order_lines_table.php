2b07d5866fd30b9f13eed98f575449b992e60d8a
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-02-27 15:30:27 +0100

    Model + migration + Controller OrderLine

---
+++ database/migrations/2020_02_27_151832_create_order_lines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('quantity');
+            $table->decimal('price',10, 2);
+            $table->decimal('vat',5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+}
