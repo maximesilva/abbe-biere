457f767a1a42694dc4e7c0f7a07c60de72a42114
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-03-11 16:56:13 +0100

    main admin product ajout container

--- resources/views/admin_product.blade.php
+++ resources/views/admin_product.blade.php
@@ -1,52 +1,49 @@
 @extends('layout')
 
 @section('main')
+    <div class="container">
+        <form method="post">
+            @csrf
+            <div class="form-group">
+                <label for="category">Categorie</label>
+                <select class="form-control" name="category" id="exampleFormControlSelect1">
+                    <option value="1">Blondes</option>
+                    <option value="2">Brunes</option>
+                    <option value="3">Ambrées</option>
+                    <option value="4">Fruitées</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" id="name">
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input type="text" class="form-control" name="description" id="description">
+            </div>
+            <div class="form-group">
+                <label for="price">Prix</label>
+                <input type="number" class="form-control" name="price" id="price">
+            </div>
+            <div class="form-group">
+                <label for="volume">Volume</label>
+                <input type="number" class="form-control" name="volume" id="volume">
+            </div>
+            <div class="form-group">
+                <label for="vat">TVA</label>
+                <input type="number" class="form-control" name="vat" id="vat">
+            </div>
+            <div class="form-group">
+                <label for="stock">Stock</label>
+                <input type="number" class="form-control" name="stock" id="stock">
+            </div>
+            <div class="form-group">
+                <label for="weight">Weight</label>
+                <input type="number" class="form-control" name="weight" id="weight">
+            </div>
 
-
-    <form method="post">
-@csrf
-        <div class="form-group">
-            <label for="category">Categorie</label>
-            <select class="form-control" name="category" id="exampleFormControlSelect1">
-                <option value="1">Blondes</option>
-                <option value="2">Brunes</option>
-                <option value="3">Ambrées</option>
-                <option value="4">Fruitées</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" name="name" id="name">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input type="text" class="form-control" name="description" id="description">
-        </div>
-        <div class="form-group">
-            <label for="price">Prix</label>
-            <input type="number" class="form-control" name="price" id="price">
-        </div>
-        <div class="form-group">
-            <label for="volume">Volume</label>
-            <input type="number" class="form-control" name="volume" id="volume">
-        </div>
-        <div class="form-group">
-            <label for="vat">TVA</label>
-            <input type="number" class="form-control" name="vat" id="vat">
-        </div>
-        <div class="form-group">
-            <label for="stock">Stock</label>
-            <input type="number" class="form-control" name="stock" id="stock">
-        </div>
-        <div class="form-group">
-            <label for="weight">Weight</label>
-            <input type="number" class="form-control" name="weight" id="weight">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Créer</button>
-    </form>
-
-
-
+            <button type="submit" class="btn btn-primary">Créer</button>
+        </form>
+    </div>
 
 @endsection
