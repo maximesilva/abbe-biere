6f82415abc99e83df116f01b8db8398842844b1b
roy-amelie <roydorelon.amelie@gmail.com>
2020-03-17 16:52:29 +0100

    creation card horizontale

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,6 +3,22 @@
 @section('main')
 
     <div class="container text-center align-items-center">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <p class="card-text">{{ $product->volume }} ml , TVA {{ $product->vat }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <div class="card" style="width: 18rem;">
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
