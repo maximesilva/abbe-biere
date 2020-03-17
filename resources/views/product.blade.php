9c21913cca84271b67e0c95f117ad3cf3661a62e
roy-amelie <roydorelon.amelie@gmail.com>
2020-03-17 13:28:02 +0100

    mise en place page view_product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,6 +2,24 @@
 
 @section('main')
 
-   {{ $product->name }}
+    <div class="container text-center align-items-center">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $product->name }}</h5>
+                <p class="card-text">{{ $product->description }} </p>
+                <p class="card-text">{{ $product->price }} € </p>
+                <p class="card-text">{{ $product->volume }} ml </p>
+                <p class="card-text">TVA {{ $product->vat }} </p>
+                <p class="card-text">Stock restant : {{ $product->stock }} </p>
+                <form>
+                    <label for="quantity"> Quantité </label>
+                    <select type="number" id="quantity" name="quantity"></select>
+                    <a href="#" class="btn btn-primary">ajouter au panier</a>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
