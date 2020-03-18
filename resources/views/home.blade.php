5627df0331e2b3e3468837bfe6833d0f6f218ab2
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-03-18 13:44:01 +0100

    ajout img biere

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,20 @@
     <main>
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-                @for ($product = 0; $product < 6; $product++)
+                @for ($i = 4; $i < $product->id; $i++)
                 <div class="carousel-item active">
-                    <img class="d-block" src="img/fond_jpg.jpg" alt="First slide">
+                    <img class="d-block" src="{{ $product->name }}" alt="First slide">
                 </div>
                 @endfor
-                {{--<div class="carousel-item">
-                    <img class="d-block" src="img/brune.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block align-middle" src="img/ambree.jpg" alt="Third slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block align-middle" src="img/fruitee.jpg" alt="Third slide">
-                </div>--}}
+{{--                <div class="carousel-item">--}}
+{{--                    <img class="d-block" src="img/brune.jpg" alt="Second slide">--}}
+{{--                </div>--}}
+{{--                <div class="carousel-item">--}}
+{{--                    <img class="d-block align-middle" src="img/ambree.jpg" alt="Third slide">--}}
+{{--                </div>--}}
+{{--                <div class="carousel-item">--}}
+{{--                    <img class="d-block align-middle" src="img/fruitee.jpg" alt="Third slide">--}}
+{{--                </div>--}}
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
